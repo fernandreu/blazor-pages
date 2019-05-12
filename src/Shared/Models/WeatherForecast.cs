812070e9a5fcbfeab27333d03daa412d776b7b4f
Fernando <2480661+fernandreu@users.noreply.github.com>
2019-05-12 11:11:16 +0100

    Move WeatherForecast to Shared project

---
+++ src/Shared/Models/WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BlazorPages.Shared.Models
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
