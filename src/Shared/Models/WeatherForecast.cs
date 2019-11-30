f864f909372eead924f2dbf70df7e1e3ec4a8465
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2019-11-30 01:22:01 +0000

    Align files with latest template

--- src/Shared/Models/WeatherForecast.cs
+++ src/Shared/Models/WeatherForecast.cs
@@ -8,7 +8,7 @@ namespace BlazorPages.Shared.Models
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
     }
